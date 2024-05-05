fd68c80874bbade2b8622aacb23a0ec8580cc7c0
David Mostoller <dmostoller@gmail.com>
2024-05-05 04:14:11 -0400

    mmm

--- client/src/components/PaintingModal.js
+++ client/src/components/PaintingModal.js
@@ -8,7 +8,7 @@ return (
 <div className="ui centered grid">
     <div className="ui inverted card" style={{width: "100%", margin: "10px"}}>
         <div style={{padding: "10px"}}>
-            <img className="ui image centered" src={painting.image} alt={painting.name} style={{width: "100%"}}></img>
+            <img className="ui massive image centered" src={painting.image} alt={painting.name} style={{width: "100%"}}></img>
         </div>
     </div>
 </div> 
