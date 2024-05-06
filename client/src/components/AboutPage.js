cb510a56acf2800abbc9c9b383e40da6d7ff6181
David Mostoller <dmostoller@gmail.com>
2024-05-06 02:06:58 -0400

    centered about

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -5,7 +5,7 @@ export default function AboutPage () {
 
 return (
     <div className="ui container" style={{minHeight:"100vh"}}>
-    <div style={{width: "100%", margin: "auto"}} className="ui card">
+    <div style={{width: "100%", margin: "auto"}} className="ui centered card">
         <div className="image">
             <img className="ui huge image" src="./images/slider2.jpg" alt="Yasmin Mostoller at the Jed William's Gallery"/>
         </div>
@@ -23,16 +23,16 @@ return (
             <div>
                 {/* <a href="https://www.facebook.com/yasminmostollerart" target="none"><img style={{float: "left", padding:"5px"}} src="./icons/facebook.svg" alt="Facebook"></img></a>
                 <a href="https://www.instagram.com/yasminnunsy/" target="none"><img style={{float: "left", padding:"5px"}} src="./icons/instagram.svg" alt="Instagram"></img></a> */}
-                <Link to="https://www.facebook.com/yasminmostollerart" target="blank"  className="ui circular facebook icon button" style={{float: "left", marginTop: "5px", marginRight: "5px"}}>
+                <Link to="https://www.facebook.com/yasminmostollerart" target="blank"  className="ui circular facebook icon button" style={{marginTop: "5px", marginRight: "5px"}}>
                     <i className="facebook icon"></i>
                 </Link> 
-                <Link to="https://www.instagram.com/yasminnunsy/" target="blank"  className="ui circular icon purple button" style={{float: "left", marginTop: "5px", marginRight: "5px"}}>
+                <Link to="https://www.instagram.com/yasminnunsy/" target="blank"  className="ui circular icon purple button" style={{marginTop: "5px", marginRight: "5px"}}>
                     <i className="instagram icon"></i>
                 </Link>
 
             
             </div>
-            <div style={{padding: "10px", float: "right"}}> 
+            <div style={{padding: "10px"}}> 
                 <Link to="/contact" className="ui circular button small teal">Contact Me</Link>
             </div>
         </div>
