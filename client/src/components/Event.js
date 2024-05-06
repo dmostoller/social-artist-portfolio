737a75564e02992b65952e9145af63584083e66a
David Mostoller <dmostoller@gmail.com>
2024-05-06 01:35:30 -0400

    event modal fix2

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useUser } from "../context/user";
 import { useAdmin } from "../context/admin.js"
