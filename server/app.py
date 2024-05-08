a59ca290a5cc8bc4451a686e07791762cfd06990
David Mostoller <dmostoller@gmail.com>
2024-05-07 23:51:37 -0400

    routes again

--- server/app.py
+++ server/app.py
@@ -33,6 +33,7 @@ CORS(app)
 
 # Views go here!
 @app.route("/", defaults={'path': ''})
+@app.route("/<string:path>")
 @app.route("/<path:path>")
 def index(path):
     return render_template("index.html")
